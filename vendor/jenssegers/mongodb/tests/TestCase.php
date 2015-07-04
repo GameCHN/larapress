cdc37869deac5d10d71976e4899f07996a40bce0
Mo <me@zdiy.net>
2015-07-04 09:37:48 +0000

    vendor

---
+++ vendor/jenssegers/mongodb/tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+class TestCase extends Orchestra\Testbench\TestCase {
+
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            'Jenssegers\Mongodb\MongodbServiceProvider',
+            'Jenssegers\Mongodb\Auth\PasswordResetServiceProvider',
+        ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  Illuminate\Foundation\Application    $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // reset base path to point to our package's src directory
+        //$app['path.base'] = __DIR__ . '/../src';
+
+        $config = require 'config/database.php';
+
+        $app['config']->set('database.default', 'mongodb');
+        $app['config']->set('database.connections.mysql', $config['connections']['mysql']);
+        $app['config']->set('database.connections.mongodb', $config['connections']['mongodb']);
+
+        $app['config']->set('auth.model', 'User');
+        $app['config']->set('cache.driver', 'array');
+    }
+
+}
---
+++ vendor/jenssegers/mongodb/tests/models/Soft.php
@@ -0,0 +1,14 @@
+<?php
+
+use Jenssegers\Mongodb\Model as Eloquent;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+
+class Soft extends Eloquent {
+
+    use SoftDeletes;
+
+    protected $collection = 'soft';
+    protected static $unguarded = true;
+    protected $dates = ['deleted_at'];
+
+}
